74391543907aba50275f4a5f1d61b3f0145a17d7
Alexandr Filipov <afilipov92@gmail.com>
2015-02-18 18:39:42 +0300

    add rotation road signs

--- javascript/map.js
+++ javascript/map.js
@@ -41,24 +41,46 @@
         //отображение знаков из бд
         function addSign(pointData) {
             var folder = pointData.number.substr(0, 1);
-            var image = 'images/road_signs/' + folder + '/' + pointData.number + '.png' + '?angle=' + pointData.rotation;
+            var image = 'images/road_signs/' + folder + '/' + pointData.number + '.png' + '#id=' + pointData.id;
             var myLatLng = new google.maps.LatLng(pointData.latitude, pointData.longitude);
             var marker = new google.maps.Marker({
                 position: myLatLng,
                 map: map,
-                icon: image,
+                icon: {
+                    url: image,
+                    scaledSize: new google.maps.Size(40, 40),
+                    size: new google.maps.Size(60,60),
+                    origin: new google.maps.Point(-10,-10)
+                },
                 draggable: true,
                 title: pointData.id
             });
+
             google.maps.event.addListener(marker, 'rightclick', function() {
-                document.getElementById('form').reset();
                 $('#id').attr('value', pointData.id);
                 $('#number').attr('value', pointData.number);
                 $('#latitude').attr('value', pointData.latitude);
                 $('#longitude').attr('value', pointData.longitude);
                 $('#rotation').attr('value', pointData.rotation);
                 $('#form-signs').show();
+                rotateAngle(pointData.rotation, marker.title);
                 setMarker(marker);
+            });
+
+            google.maps.event.addListener(map, 'click', function () {
+                rotateAngle(pointData.rotation, marker.title);
+            });
+
+            updateMarkerPosition(marker.getPosition());
+
+            //изменение координат в форме при перетаскивании маркера
+            google.maps.event.addListener(marker, 'drag', function () {
+                updateMarkerPosition(marker.getPosition());
+            });
+
+            //устанавливает центр экрана по расположения маркера, после окончания перетаскивания
+            google.maps.event.addListener(marker, 'dragend', function () {
+                map.panTo(marker.getPosition());
             });
         }
 
@@ -86,7 +108,8 @@
             placeMarker.marker = new google.maps.Marker({
                 position: position,
                 map: map,
-                draggable: true
+                draggable: true,
+                title: ''
             });
             map.panTo(position);
         }
@@ -110,7 +133,7 @@
             var id = $(this).attr('id');
             $('#number').attr('value', id).focus();
             var folder = id.substr(0, 1);
-            var image = 'images/road_signs/' + folder + '/' + id + '.png';
+            var image = 'images/road_signs/' + folder + '/' + id + '.png' + '#id=' + marker.title;
             var markerIcon = {
                 scaledSize: new google.maps.Size(40, 40),
                 url: image
@@ -121,3 +144,24 @@
 
     google.maps.event.addDomListener(window, 'load', initialize);
 })();
+
+document.onkeydown = function checkKeycode(event)
+{
+    var ob = $('#rotation');
+    var angle;
+    if(event.which == 67){
+        angle = parseInt(ob.attr('value')) + 10;
+    } else if (event.which == 90) {
+        angle = parseInt(ob.attr('value')) - 10;
+    }
+    if(event.which == 90 || event.which == 67) {
+        rotateAngle(angle, $('#id').attr('value'));
+        ob.attr('value', angle);
+    }
+}
+
+function rotateAngle(angle, id) {
+    var rotate = "rotate(" + angle + "deg)";
+    var a = "img[src$='id=" +  id + "']";
+    $(a).css('transform', rotate);
+}
